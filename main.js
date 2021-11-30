b9d7cb856e0ba5391c4853282f9c31765fa5fc37
Austin Flatt <austindflatt@gmail.com>
2021-11-29 21:46:10 -0500

    set up button function

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 const apiInput = document.querySelector('#api-key');
 const addressInput = document.querySelector('#address');
 const noradInput = document.querySelector('#norad');
+const button = document.querySelector('#search');
+
+button.addEventListener('click', function() {
+
+});
